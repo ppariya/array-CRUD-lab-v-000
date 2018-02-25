1a185d11f4f5c1629b0a0caef46ef79ad25e3438
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-02-25 06:38:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  new = ["fah", "james",love","forever"]
+  new = ["fah", "james","love","forever"]
 end
 
 def add_element_to_end_of_array(array, element)
